5c2294723adb62452321a7afd36832020098b4c8
JEONG HYUN SEOK <hyunseok.jeong@annotation-ai.com>
2023-06-09 00:27:36 +0900

    feat: simple chatgpt

--- main.go
+++ main.go
@@ -2,15 +2,73 @@ package main
 
 import (
 	"bufio"
+	"context"
 	"fmt"
 	"os"
+	"strings"
+
+	"github.com/sashabaranov/go-openai"
 )
 
+const (
+	Reset  = "\033[0m"
+	Red    = "\033[31m"
+	Green  = "\033[32m"
+	Yellow = "\033[33m"
+	Blue   = "\033[34m"
+	Purple = "\033[35m"
+	Cyan   = "\033[36m"
+	Gray   = "\033[37m"
+	White  = "\033[97m"
+)
+
+var (
+	OPENAI_API_KEY string
+)
+
+func cPrint(color, msg string) string {
+	return fmt.Sprint(color + msg + Reset)
+}
+
+func init() {
+	OPENAI_API_KEY = os.Getenv("OPENAI_API_KEY")
+}
 func main() {
+
+	client := openai.NewClient(OPENAI_API_KEY)
+	messages := make([]openai.ChatCompletionMessage, 0)
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Println("Conversation")
+	fmt.Println("---------------------")
+
 	for {
-		reader := bufio.NewReader(os.Stdin)
-		fmt.Print("Enter text: ")
+		fmt.Print("-> ")
 		text, _ := reader.ReadString('\n')
-		fmt.Println("You entered: ", text)
+		// convert CRLF to LF
+		text = strings.Replace(text, "\n", "", -1)
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleUser,
+			Content: text,
+		})
+
+		resp, err := client.CreateChatCompletion(
+			context.Background(),
+			openai.ChatCompletionRequest{
+				Model:    openai.GPT3Dot5Turbo,
+				Messages: messages,
+			},
+		)
+
+		if err != nil {
+			fmt.Printf("ChatCompletion error: %v\n", err)
+			continue
+		}
+
+		content := resp.Choices[0].Message.Content
+		messages = append(messages, openai.ChatCompletionMessage{
+			Role:    openai.ChatMessageRoleAssistant,
+			Content: content,
+		})
+		fmt.Println(content)
 	}
 }
